ea4aecdf7e3137bf499446645ac8def120a84742
Josh Kalov <jkalov@gmail.com>
2017-10-02 13:18:36 -0500

    trying styling similar to server dashboard

--- js/main.js
+++ js/main.js
@@ -4,16 +4,26 @@ $(document).ready(function () {
     var items = [];
     var listHTML = ''
     $.each(data, function(key, val) {
+      console.log('val.map_id = ' + val.map_id)
       var map_id = val.map_id.replace(/[& ]/g, '+')
-      listHTML += "<li id='" + map_id + "'><strong><a href='https://fpcc-staging.smartchicagoapps.org/#/?" + val.type + "=" + map_id + "'>" + val.name + "</a></strong><ul>"
+      listHTML += "<ul class='alerts' id='" + map_id + "'><strong><a href='https://fpcc-staging.smartchicagoapps.org/#/?" + val.type + "=" + map_id + "'>" + val.name + "</a></strong>"
       $.each(val.alerts, function(key, alert) {
         if (alert.end_date === null) {
           alert.end_date = 'No end date set'
         }
-        listHTML += "<li><strong>" + alert.start_date + ' - ' + alert.end_date + "</strong>: " + alert.description + "</li>"
+        var classType = "warning"
+        var alertTypeName = "ALERT"
+        if (alert.alert_type == "closure") {
+          classType = "danger"
+          alertTypeName = "CLOSURE"
+        }
+
+        listHTML += "<li class='alerts bg-" + classType + "'><span class='label label-" + classType + "'>" + alertTypeName + "</span> <strong>" + alert.start_date + " - " + alert.end_date + "</strong>: " + alert.description + "</li>"
       })
-      listHTML += "</ul></li>"
+      listHTML += "</ul>"
     });
+
+
    
     $('#alertlist').html(listHTML)
     // $( "<ul/>", {
