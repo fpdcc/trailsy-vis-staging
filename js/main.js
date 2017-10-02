44643c1f40c4517faca33e2fd264cb6374562910
Josh Kalov <jkalov@gmail.com>
2017-10-02 13:43:29 -0500

    add global alerts

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
 $(document).ready(function () {
   // Fetch our events
-  $.getJSON( 'https://fpcc-staging.smartchicagoapps.org/alerts/list.json', function ( data ) {
+  var url = 'https://fpcc-staging.smartchicagoapps.org/alerts/list.json'
+  url = 'http://localhost:8080/alerts/list.json'
+  $.getJSON( url, function ( data ) {
     var items = [];
     var listHTML = ''
-    $.each(data, function(key, val) {
+    var globalAlerts = data['global']
+    var locationAlerts = data['locations']
+    if (globalAlerts.length > 0) {
+      listHTML += "<ul class='alerts' id='global'><strong>Global Alerts</strong>"
+      $.each(globalAlerts, function(key, alert) {
+        if (alert.end_date === null) {
+          alert.end_date = 'No end date set'
+        }
+        var classType = "warning"
+        var alertTypeName = "ALERT"
+        if (alert.alert_type == "closure") {
+          classType = "danger"
+          alertTypeName = "CLOSURE"
+        }
+        listHTML += "<li class='alerts bg-" + classType + "'><span class='label label-" + classType + "'>" + alertTypeName + "</span> <strong>" + alert.start_date + " - " + alert.end_date + "</strong>: " + alert.description + "</li>"
+      });
+      listHTML += "</ul>"
+    }
+    
+
+    $.each(locationAlerts, function(key, val) {
       console.log('val.map_id = ' + val.map_id)
       var map_id = val.map_id.replace(/[& ]/g, '+')
       listHTML += "<ul class='alerts' id='" + map_id + "'><strong><a href='https://fpcc-staging.smartchicagoapps.org/#/?" + val.type + "=" + map_id + "'>" + val.name + "</a></strong>"
@@ -23,8 +45,6 @@ $(document).ready(function () {
       listHTML += "</ul>"
     });
 
-
-   
     $('#alertlist').html(listHTML)
     // $( "<ul/>", {
     //   "class": "my-new-list",
